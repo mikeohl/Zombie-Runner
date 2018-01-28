dae6d6bcaa10007cb5082f14623cfe0ca7f9bcc2
mikeohl <mikeohl@live.com>
2018-01-28 03:37:06 -0800

    Script for respawning player

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public Transform playerSpawnPoints;
+    public bool respawn;
+
+    private Transform [] spawnPoints;
+
+	// Use this for initialization
+	void Start () {
+        //GameObject spawnPoints = GameObject.FindGameObjectWithTag("Player Spawn Points");
+        spawnPoints = playerSpawnPoints.GetComponentsInChildren<Transform>();
+        Debug.Log(spawnPoints.Length);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (respawn)
+        {
+            respawn = false;
+            ReSpawn();
+        }
+	}
+
+    public void ReSpawn()
+    {
+        int spawnPoint = Random.Range(1, spawnPoints.Length);
+        transform.position = spawnPoints[spawnPoint].position;
+    }
+}
