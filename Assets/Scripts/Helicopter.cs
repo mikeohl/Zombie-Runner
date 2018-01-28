cc529f0fc0c4dfd4957be6cedcb908d27b71a0ec
mikeohl <mikeohl@live.com>
2018-01-28 03:36:47 -0800

    Script to control helicopter calls

---
+++ Assets/Scripts/Helicopter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Helicopter : MonoBehaviour {
+
+    public AudioClip callSound;
+
+    private bool heliCalled = false;
+    //private AudioSource radio;
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+        //audioSource = 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Call Helicopter") && !heliCalled)
+        {
+            Debug.Log("Called Helicopter");
+            heliCalled = true;
+            audioSource.clip = callSound;
+            audioSource.Play();
+        }
+	}
+}
