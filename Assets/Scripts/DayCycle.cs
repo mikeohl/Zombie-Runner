49ed062c2b5d842f4454548c904208dba1a15fba
mikeohl <mikeohl@live.com>
2018-01-28 03:36:00 -0800

    Script to control day/night cycle

---
+++ Assets/Scripts/DayCycle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCycle : MonoBehaviour {
+
+    [Tooltip ("Gametime minutes that pass per Realtime seconds: Default = 1")]
+    public float timeScale = 1;
+    private Quaternion startRotation;
+
+	// Use this for initialization
+	void Start () {
+        startRotation = transform.rotation;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float angleThisFrame = .25f * Time.deltaTime * timeScale;
+        transform.RotateAround(transform.position, Vector3.forward, angleThisFrame);
+	}
+}
