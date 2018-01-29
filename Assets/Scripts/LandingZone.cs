d83740dbb9e8bc3933d6b4cf64a1c3b1e96e4a88
mikeohl <mikeohl@live.com>
2018-01-29 06:05:26 -0800

    Script to set landing zone and flare

---
+++ Assets/Scripts/LandingZone.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandingZone : MonoBehaviour {
+
+    public float collisionTimeLimit = 3f;
+
+    private float deltaClear = 0f;
+    private bool landingZoneFound = false;
+	
+	// Update is called once per frame
+	void Update () {
+        deltaClear += Time.deltaTime;
+        if (deltaClear >= collisionTimeLimit && Time.realtimeSinceStartup > 10f && !landingZoneFound)
+        {
+            landingZoneFound = true;
+            SendMessageUpwards("OnFindLandingZone");
+        }
+	}
+
+    private void OnTriggerStay(Collider collider)
+    {
+        if (collider.tag != "Player")
+        {
+            // landingZoneFound = false;
+            deltaClear = 0f;
+        }
+    }
+}
