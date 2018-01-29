c150c80eef19e06158ccdf1aa395e1733c7d984d
mikeohl <mikeohl@live.com>
2018-01-29 06:04:52 -0800

    Refactored and added message sending

--- Assets/Scripts/Helicopter.cs
+++ Assets/Scripts/Helicopter.cs
@@ -4,26 +4,26 @@ using UnityEngine;
 
 public class Helicopter : MonoBehaviour {
 
-    public AudioClip callSound;
-
+    private Rigidbody rigidBody;
     private bool heliCalled = false;
-    //private AudioSource radio;
-    private AudioSource audioSource;
 
-	// Use this for initialization
-	void Start () {
-        audioSource = GetComponent<AudioSource>();
-        //audioSource = 
+    // Use this for initialization
+    void Start () {
+        rigidBody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetButtonDown("Call Helicopter") && !heliCalled)
+		if (Input.GetButtonDown("Call Helicopter"))
         {
-            Debug.Log("Called Helicopter");
-            heliCalled = true;
-            audioSource.clip = callSound;
-            audioSource.Play();
+            // Call the helicopter
         }
 	}
+
+    public void OnDispatchHelicopter ()
+    {
+        Debug.Log("Helicopter Dispatched");
+        heliCalled = true;
+        rigidBody.velocity = new Vector3(0f, 0f, 50f);
+    }
 }
