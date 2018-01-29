b3e38e0656a21c64c192a7b01bbee1b06337a765
mikeohl <mikeohl@live.com>
2018-01-29 06:04:26 -0800

    Cleaned up

--- Assets/Scripts/DayCycle.cs
+++ Assets/Scripts/DayCycle.cs
@@ -6,12 +6,6 @@ public class DayCycle : MonoBehaviour {
 
     [Tooltip ("Gametime minutes that pass per Realtime seconds: Default = 1")]
     public float timeScale = 1;
-    private Quaternion startRotation;
-
-	// Use this for initialization
-	void Start () {
-        startRotation = transform.rotation;
-	}
 	
 	// Update is called once per frame
 	void Update () {
