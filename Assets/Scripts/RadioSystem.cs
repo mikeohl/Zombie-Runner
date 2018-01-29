61818a21dc5472f724da62c8b759862600f9eede
mikeohl <mikeohl@live.com>
2018-01-29 06:06:06 -0800

    Script to play radio messages to and from helicopter

---
+++ Assets/Scripts/RadioSystem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioSystem : MonoBehaviour {
+
+    public AudioClip heliCall;
+    public AudioClip heliCallReply;
+
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnMakeInitialHeliCall ()
+    {
+        Debug.Log(name + " OnMakeInitialHeliCall");
+        audioSource.clip = heliCall;
+        audioSource.Play();
+
+        Invoke("HeliCallReply", heliCall.length + 1f);
+    }
+
+    private void HeliCallReply ()
+    {
+        audioSource.clip = heliCallReply;
+        audioSource.Play();
+        BroadcastMessage("OnDispatchHelicopter");
+    }
+}
