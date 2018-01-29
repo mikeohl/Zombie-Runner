649185f2c50a282abe9f3d61a8f474263d0f3123
mikeohl <mikeohl@live.com>
2018-01-29 06:05:10 -0800

    Script to handle inner voice messages

---
+++ Assets/Scripts/InnerVoice.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InnerVoice : MonoBehaviour {
+
+    public AudioClip whatHappened;
+    public AudioClip goodLandingZone;
+
+    private AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = whatHappened;
+        audioSource.Play();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnFindLandingZone ()
+    {
+        Debug.Log(name + " OnFindLandingZone");
+        audioSource.clip = goodLandingZone;
+        audioSource.Play();
+
+        // What for the clip to play and call method
+        Invoke("CallHeli", goodLandingZone.length + 1f);
+    }
+
+    private void CallHeli ()
+    {
+        SendMessageUpwards("OnMakeInitialHeliCall");
+    }
+}
